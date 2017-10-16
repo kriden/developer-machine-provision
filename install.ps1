afc31d6d55495218b306122a2958c8249b93b5ad
Kristof De Nolf <kristofdenolf@hotmail.com>
2017-10-16 16:43:46 +0200

    Initial commit message

---
+++ install.ps1
@@ -0,0 +1,24 @@
+write-host "Installing Chocolatey" -foregroundcolor green
+write-host "Setting execution policy ByPass" -foregroundcolor yellow
+Set-ExecutionPolicy Bypass; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+write-host "Refreshing environment variables" -foregroundcolor green
+refreshenv
+
+write-host "Configuring chocolatey cache folder" -foregroundcolor green
+New-Variable -Name CACHE_DIR -Value $HOME\ChocolateyCache
+mkdir $CACHE_DIR
+choco config set cacheLocation $CACHE_DIR
+
+write-host "Installing all programs using chocolatey" -foregroundcolor green
+choco install googlechrome firefox evernote todoist 1password atom -y
+choco install jdk8 maven git sourcetree dropbox nvm -y
+choco install superputty  docker docker-compose docker-machine virtualbox docker-kitematic -y
+
+write-host "Configuring installed packages" -foregroundcolor green
+refreshenv
+nvm install latest
+docker-machine create default
+
+write-host "Resetting chocolatey cache"
+rm -r $CACHE_DIR\*
