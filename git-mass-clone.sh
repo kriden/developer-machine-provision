6cb60e57d753b5f22a6b86bcf6e28074dcb2614a
kriden <kristofdenolf@hotmail.com>
2018-03-05 13:16:55 +0100

    Updated to latest version - support for personal repo's

--- git-mass-clone.sh
+++ git-mass-clone.sh
@@ -15,25 +15,37 @@ if(!usr || !pwd) {
   System.exit(0);
 }
 
-def authString  = "${usr}:${pwd}".getBytes().encodeBase64().toString()
-def apiString = 'https://bitbucket.org/api/2.0/repositories/capgeminidcx/?pagelen=50'
-URL apiUrl = new URL(apiString)
-def conn = apiUrl.openConnection();
-conn.setRequestProperty( "Authorization", "Basic ${authString}" )
-def response = new JsonSlurper().parseText(conn.content.text)
+class Util {
+  def cloneRepositories(name, usr, pwd, projects) {
+    println "Cloning repositories for <${name}>";
+    def authString  = "${usr}:${pwd}".getBytes().encodeBase64().toString()
+    def apiString = "https://bitbucket.org/api/2.0/repositories/${name}/?pagelen=50"
+    URL apiUrl = new URL(apiString)
+    def conn = apiUrl.openConnection();
+    conn.setRequestProperty( "Authorization", "Basic ${authString}" )
+    def response = new JsonSlurper().parseText(conn.content.text)
 
 
-response.values.each { repo ->
-  def base = repo.project.name.toLowerCase()
+    response.values.each { repo ->
+      def base = "personal"
+      if(repo.project != null) {
+        base = repo.project.name.toLowerCase()
+      }
 
-  if(projects.empty || projects.contains(base)) {
-    def url = "https://bitbucket.org/${repo.full_name}"
-    def defaultBranch = repo.mainbranch.name
-    def target = base.replaceAll(" ", "")+"/"+repo.name
+      if(projects.empty || projects.contains(base)) {
+        def url = "https://${usr}:\"${pwd}\"@bitbucket.org/${repo.full_name}"
+        def defaultBranch = repo.mainbranch.name
+        def target = base.replaceAll(" ", "")+"/"+repo.name
 
-    def proc = ["git", "clone", url, target].execute();
-    def outputStream = new StringBuffer();
-    proc.waitForProcessOutput(outputStream, System.err);
-    println outputStream.toString();
+        def proc = ["git", "clone", url, target].execute();
+        def outputStream = new StringBuffer();
+        proc.waitForProcessOutput(outputStream, System.err);
+        println outputStream.toString();
+      }
+    }
   }
 }
+
+def util = new Util();
+util.cloneRepositories("capgeminidcx", usr, pwd, projects)
+util.cloneRepositories(usr, usr, pwd, [])
